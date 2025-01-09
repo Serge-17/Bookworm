71bb93f768c45a0d073b45244b0ad6ce43ecc23d
Sergey Eliseev <sergei.eliseev17@icloud.com>
2025-01-09 17:28:38 +0300

    ver 1

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -1,21 +1,88 @@
-//
-//  ContentView.swift
-//  Bookworm
-//
-//  Created by Serge Eliseev on 14.10.2024.
-//
-
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query(sort: [
+        SortDescriptor(\Book.title),
+        SortDescriptor(\Book.author)
+    ]) var books: [Book]
+    
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(books) { book in
+                    NavigationLink(value: book) {
+                        HStack {
+                            if hasValidBook(title: book.title, author: book.author, genre: book.genre, review: book.review) {
+                                EmojiRatingView(rating: book.rating)
+                                    .font(.largeTitle)
+                                
+                                VStack(alignment: .leading) {
+                                    Text(book.title)
+                                        .font(.headline)
+                                        .foregroundStyle(book.rating == 1 ? Color.red : Color.black)
+                                    
+                                    Text(book.author)
+                                        .foregroundStyle(.secondary)
+                                        .foregroundStyle(book.author == 1 ? Color.red : Color.black)
+                                }
+                            } else {
+                                EmojiRatingView(rating: 4)
+                                    .font(.largeTitle)
+                                
+                                VStack(alignment: .leading) {
+                                    Text("Илон Маск")
+                                        .font(.headline)
+                                    Text("Уолтер Айзексон")
+                                        .foregroundStyle(.secondary)
+                                }
+                            }
+                        }
+                    }
+                }
+                .onDelete(perform: deleteBooks)
+            }
+            .navigationTitle("Bookworm")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        showingAddScreen.toggle()
+                    } label: {
+                        Label("Add Book", systemImage: "plus")
+                    }
+                }
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
+            }
+            .sheet(isPresented: $showingAddScreen) {
+                AddBookView()
+            }
+            // Переместили вызов навигации сюда
+            .navigationDestination(for: Book.self) { book in
+                DetailView(book: book)
+            }
         }
-        .padding()
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            modelContext.delete(book)
+        }
+    }
+
+    func hasValidBook(title: String, author: String, genre: String, review: String) -> Bool {
+        if title.trimmingCharacters(in: .whitespaces).isEmpty &&
+            author.trimmingCharacters(in: .whitespaces).isEmpty &&
+            review.trimmingCharacters(in: .whitespaces).isEmpty {
+            return false
+        }
+        return true
     }
 }
 
---
+++ Bookworm/EmojiRatingView.swift
@@ -0,0 +1,32 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Serge Eliseev on 15.10.2024.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            Text("😟")
+        case 2:
+            Text("🥱")
+        case 3:
+            Text("😏")
+        case 4:
+            Text("☺️")
+        default:
+            Text("🤯")
+
+        }
+    }
+}
+
+#Preview {
+    EmojiRatingView(rating: 3)
+}
